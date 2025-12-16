60e2597ae3c99e53d7d07c89125b21a9f5f09567
Cay <neivesten@outlook.com>
2025-12-16 21:47:03 +0100

    Defined Clip and Damage percentage attributes

--- src/yawg/models/attributes.py
+++ src/yawg/models/attributes.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum, auto
+from typing import Optional
 
-#TODO: reseach actual in-game types
+
 class StatValueType(Enum):
     PERCENT = auto()
     FLAT = auto()
@@ -12,13 +13,35 @@ class StatValueType(Enum):
 @dataclass
 class AttributeDef:
     """
-    Generic Weaponm Attribute interface.
+    Generic Weapon Attribute interface.
     Example: damage%, extra double jump, can random crit 
     """
     id: str
     name: str
     description: str
     value_type: StatValueType
+    min_value: Optional[float] = None
+    max_value: Optional[float] = None
+    base_weight: float = 1.0
 
-    # Add min max?
-    base_weight: float
+    def get_description(self):
+        return self.description
+
+DAMAGE_PERCENT = AttributeDef(
+    id="damage_percent",
+    name="Damage modifier",
+    description="Modifies the damage dealt by a percentage.",
+    value_type=StatValueType.PERCENT,
+)
+
+CLIP_SIZE_PERCENT = AttributeDef(
+    id="clip_size_percent",
+    name="Clip size",
+    description="Modifies the ammo count in the clip by a percentage.",
+    value_type=StatValueType.PERCENT,
+)
+
+ATTRIBUTE_LIBRARY = {
+    DAMAGE_PERCENT.id: DAMAGE_PERCENT,
+    CLIP_SIZE_PERCENT.id: CLIP_SIZE_PERCENT,
+}
